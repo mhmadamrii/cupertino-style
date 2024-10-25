659befdb4e7e0f3fbe10398ba98e1828deea5d01
mhmadamrii <interceptorghost4@gmail.com>
2024-10-26 03:39:50 +0700

    thx

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
-import 'package:cupertino_style/screeens/home/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(
-      ProviderScope(
-        child: const MyApp(),
-      ),
-    );
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,7 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Scaffold(
+        body: Text(
+          "My App",
+        ),
+      ),
     );
   }
 }
